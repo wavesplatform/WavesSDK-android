9f1db0d182501561cf290e4ab4935b0383605d3e
Aleksandr Ershov <aershov@wavesplatform.com>
2019-09-04 18:43:48 +0300

    Parcel Tx

--- wavesplatform/src/main/java/com/wavesplatform/sdk/model/response/node/transaction/InvokeScriptTransactionResponse.kt
+++ wavesplatform/src/main/java/com/wavesplatform/sdk/model/response/node/transaction/InvokeScriptTransactionResponse.kt
@@ -1,22 +1,79 @@
 package com.wavesplatform.sdk.model.response.node.transaction
 
+import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import com.wavesplatform.sdk.keeper.interfaces.KeeperTransactionResponse
 import com.wavesplatform.sdk.model.request.node.BaseTransaction
 import com.wavesplatform.sdk.model.request.node.InvokeScriptTransaction
-import kotlinx.android.parcel.Parcelize
 
 /**
  * See [com.wavesplatform.sdk.model.request.node.InvokeScriptTransaction]
  */
-@Parcelize
 class InvokeScriptTransactionResponse(@SerializedName("feeAssetId")
-                                          var feeAssetId: String?,
+                                      var feeAssetId: String?,
                                       @SerializedName("dApp")
-                                          var dApp: String,
+                                      var dApp: String,
                                       @SerializedName("call")
-                                          var call: InvokeScriptTransaction.Call?,
+                                      var call: InvokeScriptTransaction.Call?,
                                       @SerializedName("payment")
-                                          var payment: Array<InvokeScriptTransaction.Payment>)
-    : BaseTransactionResponse(type = BaseTransaction.SCRIPT_INVOCATION), Parcelable, KeeperTransactionResponse
+                                      var payment: Array<InvokeScriptTransaction.Payment>)
+    : BaseTransactionResponse(type = BaseTransaction.SCRIPT_INVOCATION), Parcelable, KeeperTransactionResponse {
+
+    constructor(parcel: Parcel) : this(
+            feeAssetId = parcel.readString() ?: "",
+            dApp = parcel.readString() ?: "",
+            call = parcel.readParcelable(InvokeScriptTransaction.Call::class.java.classLoader),
+            payment = getPayment(parcel)) {
+
+        id = parcel.readString()
+        sender = parcel.readString() ?: ""
+        senderPublicKey = parcel.readString() ?: ""
+        timestamp = parcel.readLong()
+        fee = parcel.readLong()
+        // chainId = parcel.readByte()
+        version = parcel.readByte()
+        parcel.readStringList(proofs)
+        signature = parcel.readString() ?: ""
+        height = parcel.readLong()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(feeAssetId)
+        parcel.writeString(dApp)
+        parcel.writeParcelable(call, flags)
+        parcel.writeTypedArray(payment, flags)
+
+        parcel.writeString(id)
+        parcel.writeString(sender)
+        parcel.writeString(senderPublicKey)
+        parcel.writeLong(timestamp)
+        parcel.writeLong(fee)
+        // parcel.writeByte(chainId ?: WavesSdk.getEnvironment().chainId)
+        parcel.writeByte(version)
+        parcel.writeStringList(proofs)
+        parcel.writeString(signature)
+        parcel.writeLong(height ?: 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<InvokeScriptTransactionResponse> {
+
+        fun getPayment(parcel: Parcel): Array<InvokeScriptTransaction.Payment> {
+            val tempPayment = arrayOf<InvokeScriptTransaction.Payment>()
+            parcel.readTypedArray(tempPayment, InvokeScriptTransaction.Payment.CREATOR)
+            return tempPayment
+        }
+
+        override fun createFromParcel(parcel: Parcel): InvokeScriptTransactionResponse {
+            return InvokeScriptTransactionResponse(parcel)
+        }
+
+        override fun newArray(size: Int): Array<InvokeScriptTransactionResponse?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
