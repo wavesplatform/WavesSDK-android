dc9125daabc7751241ef644bea23b9e7222f342f
Aleksandr Ershov <4metall@gmail.com>
2019-12-03 19:27:16 +0300

    Update URL_MATCHER

--- wavesplatform/src/main/java/com/wavesplatform/sdk/utils/WavesConstants.kt
+++ wavesplatform/src/main/java/com/wavesplatform/sdk/utils/WavesConstants.kt
@@ -8,7 +8,7 @@ class WavesConstants {
 
         const val URL_NODE = "https://nodes.wavesnodes.com/"
         const val URL_DATA = "https://api.wavesplatform.com/"
-        const val URL_MATCHER = "https://matcher.wavesplatform.com/"
+        const val URL_MATCHER = "https://matcher.waves.exchange/"
 
         const val URL_NODE_TEST = "https://pool.testnet.wavesnodes.com/"
         const val URL_DATA_TEST = "https://api.testnet.wavesplatform.com/"
