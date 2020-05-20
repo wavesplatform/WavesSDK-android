af2c8939451fdc8e7cf0a3576e929504f6a8af3b
Anton Prozorov <avprozorov@gmail.com>
2020-05-20 11:31:49 +0300

    added Throwable.asRetrofitException() as extension function

---
+++ wavesplatform/src/main/java/com/wavesplatform/sdk/utils/ThrowableExtensions.kt
@@ -0,0 +1,29 @@
+package com.wavesplatform.sdk.utils
+
+import com.wavesplatform.sdk.net.NetworkException
+import retrofit2.HttpException
+import java.io.IOException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import java.util.concurrent.TimeoutException
+
+fun Throwable.asRetrofitException(): NetworkException {
+
+    // Non-200 http error
+    if (this is HttpException) {
+        val response = this.response()
+        return NetworkException.httpError(response.raw().request()
+            .url().toString(), response, null)
+    }
+
+    if (this is TimeoutException
+        || this is ConnectException
+        || this is SocketTimeoutException
+        || this is UnknownHostException
+    ) {
+        return NetworkException.networkError(IOException(message, this))
+    }
+
+    return NetworkException.unexpectedError(this)
+}
