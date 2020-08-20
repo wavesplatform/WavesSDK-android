4ee3960161f2d8a56fda245c1d073052a8609ea1
Aleksandr Ershov <4metall@gmail.com>
2020-08-20 13:30:24 +0300

    import annotation

--- wavesplatform/src/main/java/com/wavesplatform/sdk/net/service/NodeService.kt
+++ wavesplatform/src/main/java/com/wavesplatform/sdk/net/service/NodeService.kt
@@ -43,6 +43,7 @@ import com.wavesplatform.sdk.model.response.node.transaction.TransferTransaction
 import io.reactivex.Observable
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
